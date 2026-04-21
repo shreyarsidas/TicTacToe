ba84f60aa79fb98e96c044b5d8f0c05743476d74
shreyarsidas <shreyarsidas@gmail.com>
2026-04-21 09:34:45 +0530

    UC2

--- TicTacToe/src/main/java/org/example/Main.java
+++ TicTacToe/src/main/java/org/example/Main.java
@@ -1,17 +1,57 @@
 package org.example;
+import java.util.Random;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+/**
+ * TicTacToe
+ * UC2: Toss to decide first player and assign symbols
+ */
+public class TicTacToe {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
+
+    /**
+     * Entry point of the program
+     */
+    public static void main(String[] args) {
+        tossAndAssignSymbols();
+        displayTossResult();
+    }
+
+    /**
+     * Performs a random toss and assigns symbols
+     */
+    static void tossAndAssignSymbols() {
+        Random rand = new Random();
+
+        // 0 → Human starts, 1 → Computer starts
+        int toss = rand.nextInt(2);
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
         }
+    }
+
+    /**
+     * Displays toss result and symbol assignment
+     */
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("You won the toss!");
+            System.out.println("You play first.");
+        } else {
+            System.out.println("Computer won the toss!");
+            System.out.println("Computer plays first.");
+        }
+
+        System.out.println("Your symbol: " + humanSymbol);
+        System.out.println("Computer's symbol: " + computerSymbol);
     }
 }
